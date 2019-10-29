11c988131767dd43d64888eb5af5c3e7827a4212
Kirill Gavrilchik <gavrilchikkirill6@gmail.com>
2019-10-30 00:09:59 +0300

    grid

--- messenger/src/App.js
+++ messenger/src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App=() =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://i.pinimg.com/736x/33/b8/69/33b869f90619e81763dbf1fccc896d8d--lion-logo-modern-logo.jpg'/>
       </header>
+      <nav className='navbar'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT26up8_h0gXF8Ucn9x008hFhNJ8bYRCU-zNTWkjVcZNbh3WM7vIQ&s'/>
+      </div>
     </div>
   );
 }
